761c40a0cba79a1158a8981dd6a5875ec8c8e9a5
MuammarRizal <muammarrizal31@gmail.com>
2024-12-14 15:39:59 +0700

    Schema & Query

---
+++ _db.js
@@ -0,0 +1,33 @@
+const books = [
+  {
+    id: "1",
+    title: "Harry Potter",
+    author: "Muammar RIzal",
+    published_at: "1992",
+    category: "Fantasy",
+    total: 20,
+  },
+  {
+    id: "2",
+    title: "Naruto Shippuden",
+    author: "Alfina Rahmalia",
+    published_at: "1993",
+    category: "Horror",
+    total: 30,
+  },
+];
+
+const members = [
+  {
+    id: "1",
+    name: "Muammar Rizal",
+    univ: "Universitas Indraprasta PGRI",
+  },
+  {
+    id: "2",
+    name: "Alfina Rahmalia",
+    univ: "Bakti 17",
+  },
+];
+
+export { members, books };
--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 import { ApolloServer } from "@apollo/server";
-import {startStandaloneServer} from '@apollo/server/standalone'
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { members, books } from "./_db.js";
 
 const typeDefs = `#graphql
-    type Query{
-        hello: String
-        greeting: String
+    type Book{
+      id: ID,
+      title: String,
+      author:  String,
+      published_at: String
+      category: String
+      total: Int
     }
-`
+
+    type Member{
+      id: ID!,
+      name: String!,
+      univ: String
+    }
+
+    type Query{
+      books :[Book]
+      members : [Member!]!
+      hello: String
+      greeting: String
+    }
+`;
 
 const resolvers = {
-    Query: {
-        hello: () => "Hello world",
-        greeting: () => "Muammar Rizal"
-    }
-}
+  Query: {
+    books: () => books,
+    members: () => members,
+    hello: () => "Hello world",
+    greeting: () => "Muammar Rizal",
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,resolvers 
+  typeDefs,
+  resolvers,
 });
 
-const {url} = await startStandaloneServer(server,{
-    listen: {port: 4000}
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
 console.log(`Server running at port ${url}`);
